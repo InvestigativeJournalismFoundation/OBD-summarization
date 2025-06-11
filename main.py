ce357bcf2d9233e19ff62bd77d75302fc42e66cd
Alan Wei <56909006+AlanWei0405@users.noreply.github.com>
2025-06-11 09:38:27 -0400

    Data Simulation

--- main.py
+++ main.py
@@ -3,14 +3,11 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# data simulation for generated summarizations and ground truths
+def simulate_data():
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    simulate_data
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
